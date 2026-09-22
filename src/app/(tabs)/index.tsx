a175ecb0a2fc4e2c6e4cea18634e850c9244b552
humam <humamsukma2@gmail.com>
2026-09-22 09:19:02 +0700

    chore: restrukturisasi folder untuk Expo Router

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,10 +3,10 @@
 import { View } from "react-native";
 import { useState, useEffect } from "react";
 
-import WeatherCard from "../components/WeatherCard";
-import SearchBox from "../components/SearchBox";
-import RiwayatList from "../components/RiwayatList";
-import IndikatorAQI from "../components/indikatorAQI";
+import WeatherCard from "../../components/WeatherCard";
+import SearchBox from "../../components/SearchBox";
+import RiwayatList from "../../components/RiwayatList";
+import IndikatorAQI from "../../components/indikatorAQI";
 
 export default function HalamanUtama() {
   const [kotaAktif, setKotaAktif] = useState("Pekalongan");
